80d12ba019c4beedeb272d1ff5cd18bb5fb0fe91
Yusnel Rojas Garcia <yrojass@gmail.com>
2018-05-10 14:58:18 -0400

    totals and last purchase price

--- mrp_production_cost/report/mrp_production_cost_report.py
+++ mrp_production_cost/report/mrp_production_cost_report.py
@@ -9,7 +9,17 @@ class MrpProductionCost(models.AbstractModel):
 
     @api.multi
     def get_data(self, productions):
+
+        data = {
+            'lines': [],
+            'total': 0,
+            'reserved_price': 0,
+            'pending_price': 0,
+            'currency': self.env.user.company_id.currency_id
+        }
+
         product_lines = []
+        PurchaseOrder = self.env['purchase.order'].sudo()
 
         for mo in productions:
 
@@ -21,6 +31,7 @@ class MrpProductionCost(models.AbstractModel):
 
             product_line = {'order': mo, 'bom': bom, 'name': mo.product_id.name,
                             'lines': [],
+                            'product_id': mo.product_id,
                             'available_lines': [],
                             'pending_lines': [],
                             'total': 0.0,
@@ -35,9 +46,22 @@ class MrpProductionCost(models.AbstractModel):
             for move in mo.move_raw_ids:
                 product = move.product_id
 
-                price_uom = move.product_uom._compute_price(product.standard_price, move.product_uom)
+                seller_price = None
 
-                lot_names = ','.join([lot for lot in move.active_move_line_ids.mapped('lot_id').mapped('name') if lot is not False])
+                self.env.cr.execute("select purchase_order.partner_id, price_unit from purchase_order_line INNER JOIN purchase_order ON (purchase_order.id=purchase_order_line.order_id) WHERE product_id=%s AND purchase_order.state IN ('purchase', 'done') ORDER BY purchase_order.id DESC LIMIT 1" % (product.id, ))
+
+                item = self.env.cr.fetchone()
+
+                if item:
+                    seller_price = item[1]
+                elif len(product.seller_ids) > 0:
+                    seller_price = product.seller_ids[-1].price
+
+                product_price = seller_price if seller_price is not None else product.standard_price
+                price_uom = move.product_uom._compute_price(product_price, move.product_uom)
+
+                lot_names = ','.join(
+                    [lot for lot in move.active_move_line_ids.mapped('lot_id').mapped('name') if lot is not False])
 
                 line = {
                     'product_id': product,
@@ -59,12 +83,18 @@ class MrpProductionCost(models.AbstractModel):
                 product_line['total_reserved_price'] += line['total_reserved_price']
                 product_line['total_pending_price'] += line['total_pending_price']
 
+            data['total'] += product_line['total']
+            data['reserved_price'] += product_line['total_reserved_price']
+            data['pending_price'] += product_line['total_pending_price']
+
             product_lines += [product_line]
 
-        return product_lines
+        data['lines'] = product_lines
+
+        return data
 
     @api.model
     def get_report_values(self, docids, data=None):
         productions = self.env['mrp.production'].browse(docids)
-        res = self.get_data(productions)
-        return {'lines': res}
+        data = self.get_data(productions)
+        return data
